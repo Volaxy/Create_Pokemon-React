b8037880a08d793291aed7c6483bd16ea7a0bf35
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-03-20 13:01:54 -0300

    ADD - Header and Pokeball

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import Pokeball from "../Pokeball/Pokeball";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <Pokeball />
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Pokeball/Pokeball.js
@@ -0,0 +1,11 @@
+import "./Pokeball.css";
+
+const Pokeball = () => {
+    return (
+        <div className="external-pokeball">
+            <div className="internal-pokeball"></div>
+        </div>
+    )
+};
+
+export default Pokeball;
