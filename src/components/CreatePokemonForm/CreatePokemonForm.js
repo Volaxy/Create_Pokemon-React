d2f794254854f961a897d5839c48c9a931e16cc6
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-03-23 17:03:29 -0300

    ADD - Value of the Inputs

--- src/components/CreatePokemonForm/CreatePokemonForm.js
+++ src/components/CreatePokemonForm/CreatePokemonForm.js
@@ -1,17 +1,65 @@
+import { useState } from "react";
 import ComboBox from "../ComboBox/ComboBox";
 import Input from "../Input/Input";
 import "./CreatePokemonForm.css";
 
 const CreatePokemonForm = () => {
+    const [name, setName] = useState("");
+    const [hp, setHp] = useState("");
+    const [atk, setAtk] = useState("");
+    const [def, setDef] = useState("");
+    const [url, setUrl] = useState("");
+
     return (
         <form className="create-pokemon-form">
-            <Input backgroundColor="#FFFFF2" boxShadowColor="rgba(255, 201, 65, 0.6)" classes="input--subsection" label="Name" />
+            <Input
+                backgroundColor="#FFFFF2"
+                boxShadowColor="rgba(255, 201, 65, 0.6)"
+                classes="input--subsection"
+                label="Name"
+                value={name}
+                onChange={value => setName(value)}
+                placeholder="Bulbasaur..."
+                required={true}
+            />
 
-            <Input backgroundColor="#F3FFF2" boxShadowColor="rgba(69, 255, 65, 0.6)" label="HP" />
-            <Input backgroundColor="#FFF2F2" boxShadowColor="rgba(255, 65, 65, 0.6)" label="ATK" />
-            <Input backgroundColor="#F2F3FF" boxShadowColor="rgba(65, 72, 255, 0.6)" classes="input--subsection" label="DEF" />
+            <Input
+                backgroundColor="#F3FFF2"
+                boxShadowColor="rgba(69, 255, 65, 0.6)"
+                label="HP"
+                value={hp}
+                onChange={value => setHp(value)}
+                placeholder="Life of the Pokemon"
+                required={true}
+            />
+            <Input
+                backgroundColor="#FFF2F2"
+                boxShadowColor="rgba(255, 65, 65, 0.6)"
+                label="ATK"
+                value={atk}
+                onChange={value => setAtk(value)}
+                placeholder="Attack of the Pokemon"
+                required={true}
+            />
+            <Input
+                backgroundColor="#F2F3FF"
+                boxShadowColor="rgba(65, 72, 255, 0.6)"
+                classes="input--subsection"
+                label="DEF"
+                value={def}
+                onChange={value => setDef(value)}
+                placeholder="Defense of the Pokemon"
+                required={true}
+            />
             
-            <Input backgroundColor="#FFEAFD" boxShadowColor="rgba(206, 65, 255, 0.6)" label="URL" />
+            <Input
+                backgroundColor="#FFEAFD"
+                boxShadowColor="rgba(206, 65, 255, 0.6)"
+                label="URL"
+                value={url}
+                onChange={value => setUrl(value)}
+                placeholder="Image of the Pokemon"
+            />
 
             <ComboBox />
 
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -6,14 +6,16 @@ const Input = (props) => {
             <label
                 className="input__label"
                 htmlFor={props.label.toLowerCase()}
-            >
-                {props.label}:
-            </label>
+            >{props.label}:</label>
 
             <input
                 className="inner-input"
                 type="text" id={props.label.toLowerCase()}
                 style={{backgroundColor: props.backgroundColor, boxShadow: `0px 0px 5px ${props.boxShadowColor}`}}
+                value={props.value}
+                onChange={event => props.onChange(event.target.value)}
+                placeholder={props.placeholder}
+                required={props.required}
             />
         </div>
     )
